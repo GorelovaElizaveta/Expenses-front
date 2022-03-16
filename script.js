00a2b455e14209db5e65b3d1b4cf1aaa89a011a7
GorelovaElizaveta <gorelova.exceedteam@gmail.com>
2022-03-16 18:12:20 +0300

    final edit

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ let inputValueNumber = "";
 let editTask = -1;
 let intermediaresult ="";
 let intermediaresultNumber ="";
-let value = -1;
+let intermediaresultDate =""
+let valueDblckTxt = -1;
+let valueDblckPrice = -1;
+let valueNewData = -1;
+let dd =""
+let flag = false;
 
 window.onload = async () => {
   inputTask = document.getElementById("add-task");
@@ -40,7 +45,7 @@ const updateValueNumb = (event) => {
 };
 
 const render = () => {
-  let count = null;
+  let count = " ";
   const global = document.getElementById("global");
   while (global.firstChild) {
     global.removeChild(global.firstChild);
@@ -59,33 +64,63 @@ const render = () => {
   allExpenses.map((value, index) => {
     const container = document.createElement("div");
     const Expens = document.createElement("div");
-    const  newDate = document.createElement("div");
-    newDate.innerText = newDateFormat();
+    Expens.className ="expenses-in-container"
+    const  newDateCont = document.createElement("div");
+    newDateCont.className ="date-in-container"
+    if(flag == true){
+    newDateCont.innerText = newDateFormat();
+      flag = false
+    } else {
+      let format = allExpenses[index].Date.split("-").reverse().join(".")
+      newDateCont.innerText = format
+    }
+    dd =  newDateCont.innerText
+    // console.log(newDateCont.innerText)
     Expens.innerText = value.Expenses + " " + "руб.";
     const imgDelete = document.createElement("img");
     container.className = "div-container-text";
     const text = document.createElement("p");
+    text.className ="text-in-container"
     text.innerText = value.text;
     
-    if (index === editTask) {                //все редактирование
-      elInput = allExpenses[index].text;
-      Price = allExpenses[index].Expenses;
+    if (index === editTask) {
+      newValueText = allExpenses[index].text;
+      newValue = allExpenses[index].Expenses;
+      ddddddDATA = dd;
+      console.log(dd)
 
+      const editDataTask =document.createElement("input");
       const newinputTask = document.createElement("input");
       const newNumberTask = document.createElement("input");
+      
+      editDataTask.type ="date"
+      editDataTask.min ="2022-01-01"
+      editDataTask.max ="2022-12-31"
       newNumberTask.type ="Number";
       newinputTask.type = "text";
+
+      editDataTask.value = dd
       newNumberTask.value = value.Expenses;
       newinputTask.value = value.text;
+
+      editDataTask.innerText = dd
       newNumberTask.innerText = value.Expenses;
       newinputTask.innerText = value.text;
+
+      intermediaresultDate= dd;
       intermediaresultNumber = value.Expenses;
       intermediaresult = value.text;
+
+      editDataTask.className = "inputDateChange"
       newNumberTask.className = "inputNumberChange";
       newinputTask.className = "inputChange";
+
       newNumberTask.addEventListener("change", newPrice);
       newinputTask.addEventListener("change", taskTxt);
+      editDataTask.addEventListener("change", dateNew)
+
       container.appendChild(newinputTask);
+      container.appendChild(editDataTask);
       container.appendChild(newNumberTask);
       const newDiv = document.createElement("div");
       container.appendChild(newDiv);
@@ -102,17 +137,43 @@ const render = () => {
       newDiv.appendChild(imgBack);
     } 
     else {
-      if (value === index) {         //dblck
+      switch(index){
+      case  valueDblckTxt:
         const newinputTask = document.createElement("input");
+        newinputTask.addEventListener("blur", onblurTxt)
+
         newinputTask.type = "text";
         newinputTask.innerText = value.text;
+        console.log(newinputTask.innerText )
         newinputTask.className = "inputChange";
         container.appendChild(newinputTask);
-
-      } else {
-        container.appendChild(text);
+        container.appendChild(newDateCont);
         container.appendChild(Expens);
+        break;
+
+        case valueDblckPrice:
+        const newNumberTask = document.createElement("input");
+        newNumberTask.type = "Number";
+        newNumberTask.innerText = value.Expenses;
+        newNumberTask.className = "inputChangeExpenses";
+        container.appendChild(text);
+        container.appendChild(newDateCont);
+        container.appendChild(newNumberTask);
+        break;
+
+        case valueNewData:
+        const newDate = document.createElement("input");
+        newDate.type = "date";
+        container.appendChild(text);
         container.appendChild(newDate);
+        container.appendChild(Expens);
+        break;
+      
+    
+      default:
+        container.appendChild(text);
+        container.appendChild(newDateCont);
+        container.appendChild(Expens);
         const noEdit = document.createElement("div");
         container.appendChild(noEdit);
         imgDelete.src = "delete.png";
@@ -120,7 +181,9 @@ const render = () => {
         imgDelete.onclick = () => {
           delTask(index);
         };
-        text.addEventListener('dblclick', () => dblclickText(index));
+        text.addEventListener('dblclick', () => dblcklText(index));
+        Expens.addEventListener('dblclick', () => dblcklExpens(index));
+        newDateCont.addEventListener('dblclick', () => dblcklnewDate(index));
         const imgEdit = document.createElement("img");
         imgEdit.src = "edit.png";
         imgEdit.onclick = () => {
@@ -131,11 +194,18 @@ const render = () => {
       }
     }
     content.appendChild(container);
-console.log(value);
   });
 };
 
 const onCklickButton = async () => {
+  if(editTask >= 0){  
+    alert("Закончите редактировние")
+  }else{
+  let dt = newDateFormat()
+  console.log(dt)
+  if(inputValueNumber <= 0){
+    alert("Расходы должны быть больше 0 рублей.")
+  } else{
   if (inputValue.trim() && inputValueNumber.trim()) {
     const resp = await fetch("http://localhost:8000/createTask", {
       method: "POST",
@@ -146,8 +216,11 @@ const onCklickButton = async () => {
       body: JSON.stringify({
         text: inputValue,
         Expenses: inputValueNumber,
+        Date: dt
+        
       }),
     });
+    flag = true;
     const result = await resp.json();
     allExpenses = result.data;
     inputValue = " ";
@@ -158,6 +231,8 @@ const onCklickButton = async () => {
   } else {
     alert("Поле не заполнено!");
   }
+};
+};
 };
 
 const delTask = async (index) => {
@@ -170,24 +245,66 @@ const delTask = async (index) => {
   render();
 };
 
-const taskTxt = (event) =>{
-  elInput = event.target.value;
+const taskTxt = (event) => {
+  newValueText = event.target.value;
 }
-const newPrice = (event) =>{
-  Price = event.target.value;
+const newPrice = (event) => {
+  newValue= event.target.value;
 }
+
+const dateNew = (event) =>{
+  ddddddDATA = event.target.value;
+
+}
+
 
 const backTask = () => {
     editTask = "-1";
     render();
 };
 
-const dblclickText = (index) => {
-  value = index;
+const dblcklText = (index) => {
+  valueDblckTxt = index;
   render();
-  ;
 };
+
+const dblcklExpens = (index) => {
+  valueDblckPrice = index;
+  render();
+};
+
+const dblcklnewDate = (index) =>{
+  valueNewData = index;
+  render();
+}
+
 
 const doneTask = async (index) => {
-
+    let {_id} = allExpenses[index];
+      const resp = await fetch(
+        `http://localhost:8000/updateTask`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            _id ,
+            text: newValueText === allExpenses[index].text ? allExpenses[index].text : newValueText,
+            Expenses: newValue ===  allExpenses[index].Expenses ? allExpenses[index].Expenses : newValue,
+            Date: ddddddDATA === dd ? dd: ddddddDATA
+          }),
+          
+        }
+      );
+      const result = await resp.json();
+      allExpenses = result.data;
+      editTask = -1;
+      dd=""
+      ddddddDATA =""
+      newValueText = '';
+      newValue =";"
+      render();
 };
+
